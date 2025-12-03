40744ee375702cbab3ff865b168fac60cba14633
BNMAMA <ksuyeon56@naver.com>
2025-12-03 17:37:51 +0900

    controller, dto 요청

---
+++ src/ch04/Axios04.jsx
@@ -0,0 +1,169 @@
+import axios from "axios";
+
+function Axios04() {
+
+    const api = axios.create({
+        baseURL: "http://localhost:8080",
+    });
+
+    const reqGetOnClick1 = () => {
+        api.get("/req/data1?a=aaaaa&b=10");
+    } 
+
+    const reqGetOnClick2 = () => {
+       api.get("/req/data2", {
+        params: {
+            a: "abc",
+            b: 10000,
+        },
+       });
+    }
+
+    const reqGetOnClick3 = () => {
+        api.get("/req/data3", {
+            params: {
+               name: "김백년",
+               age: 5,
+            }
+        })
+    }
+
+    const reqGetOnClick7 = () => {
+        api.get("/req/abc/data7/20", {
+            params: {
+                name: "김소현",
+                age: 26,
+            }
+        })
+    }
+
+    const reqPostOnClick1 = () => {
+        const data = {
+            name: "test",
+            age: 26,
+            address: "test address",
+        }
+
+        //Object -> JSON
+        const jsonData = JSON.stringify(data);
+
+        //JSON -> Object
+        const obj = JSON.parse(jsonData);
+        
+        //axios는 자동으로 JSON 변환
+        api.post("/req/data1", data);
+    }
+
+    const reqPostOnClick2 = () => {
+        api.post("/req/data2", {
+            name: "김소현",
+            age: 26 ,
+        })
+    }
+
+    const reqPostOnClick3 = () =>{
+        const inputElement = document.createElement("input");
+        inputElement.setAttribute("type", "file");
+
+        inputElement.onchange = (e) => {
+            const {files} = e.target;
+            const [file] = files;//무조건 첫번째 객체를 가지고 온다
+            //input type file 에만 있는 속성
+
+            const formData = new FormData();
+            formData.append("file", file);
+            api.post("/req/data3", formData, {
+                headers: {
+                    "Content-Type": "multipart/form-data",
+                }
+            });
+        }
+
+        inputElement.click();
+
+    }
+
+     const reqPostOnClick4 = () =>{
+        const inputElement = document.createElement("input");
+        inputElement.setAttribute("type", "file");
+
+        inputElement.onchange = (e) => {
+            const {files} = e.target;
+            const [file] = files;//무조건 첫번째 객체를 가지고 온다
+            //input type file 에만 있는 속성
+
+            const formData = new FormData();
+            formData.append("file", file);
+            formData.append("name", "김백년");
+            formData.append("age", 4);
+            
+            api.post("/req/data4", formData, {
+                headers: {
+                    "Content-Type": "multipart/form-data",
+                }
+            });
+        }
+
+        inputElement.click();
+
+    }
+
+    const reqPostOnClick5 = () => {
+        const inputElement = document.createElement("input");
+        inputElement.setAttribute("type", "file");
+        inputElement.setAttribute("multiple", true);
+                                    //여러개의 파일 받음
+        inputElement.onchange = (e) => {
+            const {files} = e.target;
+            const fileList = Array.from(files);
+
+            const formData = new FormData();
+            fileList.forEach(file => formData.append("files", file));
+
+            api.post("/req/data5", formData);
+        }
+
+        inputElement.click();
+    }
+    
+    const reqPutOnClick = () => {
+        api.put("/req/data1/30", {
+            name: "김소현",
+            age: 27,
+        });
+    }
+
+    const reqPatchOnClick = () => { //부분 수정
+        api.patch("/req/data1/30", {
+            name: "김백년",
+            
+        });
+    }
+
+    const reqDeleteOnClick = () => { //config 안의 data에 넣어야 함
+        api.delete("/req/data1/20", {
+            data: {
+                name: "김준일",
+
+            }
+        })
+    }
+
+
+    return <>
+        <button onClick={reqGetOnClick1}>reqGet1</button>
+        <button onClick={reqGetOnClick2}>reqGet2</button>
+        <button onClick={reqGetOnClick3}>reqGet3</button>
+        <button onClick={reqGetOnClick7}>reqGet7</button>
+        <button onClick={reqPostOnClick1}>reqPost1</button>
+        <button onClick={reqPostOnClick2}>reqPost2</button>
+        <button onClick={reqPostOnClick3}>reqPost3</button>
+        <button onClick={reqPostOnClick4}>reqPost4</button>
+        <button onClick={reqPostOnClick5}>reqPost5</button>
+        <button onClick={reqPutOnClick}>reqPut</button>
+        <button onClick={reqPatchOnClick}>reqPatch</button>
+        <button onClick={reqDeleteOnClick}>reqDelete10</button>
+    </>
+}
+
+export default Axios04;
