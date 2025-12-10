40dcf39e68e5813f10c0ed5ade609a57bbbecf7b
BNMAMA <ksuyeon56@naver.com>
2025-12-10 17:40:03 +0900

    login

---
+++ src/ch05/Auth02.jsx
@@ -0,0 +1,108 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+function Auth02() {
+    const [ inputValue, setInputValue ] = useState({
+        username: "",
+        password: "",
+    });
+
+    const [ inputMessage, setInputMessage ] = useState({
+        username:"",
+        password: "",
+    })
+
+    const [ signinButtonDisabled, setSigninButtonDisabled ] = useState(true);
+
+      
+    const regexs = {
+        username: {
+            regex: /^[a-z0-9_-]{5,20}$/,
+            message: "아이디: 5~20자의 영문 소문자, 숫자와 특수기호(_),(-)만 사용 가능합니다.",
+        },
+        password: {
+            regex: /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[^A-Za-z0-9])[A-Za-z0-9^A-Za-z0-9\W]{8,16}$/,
+            message: "비밀번호: 8~16자의 영문 대/소문자, 숫자, 특수문자를 사용해 주세요.",
+        },
+    }
+
+    useEffect(() => {
+        const isBlank = Object.values(inputValue).includes("");
+        const isNotValid = Object.values(inputMessage).map(message => !!message).includes(true);
+        
+        setSigninButtonDisabled(isBlank || isNotValid)
+    },[inputValue])
+
+
+    const handleInputOnChange = (e) => {
+        setInputValue({
+            ...inputValue,
+            [e.target.name]: e.target.value
+        });
+        
+        if (regexs[e.target.name].regex.test(e.target.value)) {
+            setInputMessage({
+                ...inputMessage,
+                [e.target.name]: "",
+            })
+
+        } else {
+            setInputMessage({
+                ...inputMessage,
+                [e.target.name]: regexs[e.target.name].message,
+            });
+        }
+
+            setSigninButtonDisabled(Object.values(inputMessage).map(message => !!message).includes(true));
+          
+    }
+
+    const handleSingninOnClick = () => {
+      signinRequest();
+    }
+
+    const signinRequest = async() => {
+        try {
+            const response = await axios.post("http://localhost:8080/api/auth/signin", inputValue);
+            const {accessToken} = response.data; //application access 토큰 확인!
+            localStorage.setItem("AccessToken", accessToken);
+            await getPincipalRequest();
+        } catch (error) {
+            alert(error.response.data.message);
+        }
+        
+    }
+
+     
+    const getPincipalRequest = async() => {
+        try {
+            const response = await axios.get("http://localhost:8080/api/auth/principal", {
+                headers: {
+                    "Authorization": `Bearer ${localStorage.getItem("AccessToken")}`,
+                }
+            });
+            console.log(response)
+        } catch(error){
+             console.log(error);
+        }
+           
+    }
+  
+
+
+
+    return <>
+   
+        <div>
+            <input type="text" placeholder="사용자 이름" name="username" value={inputValue.username} onChange={handleInputOnChange}/>
+            <span>{inputMessage.usernmae}</span>
+        </div>
+        <div>
+            <input type="text" placeholder="비밀번호" name="password" value={inputValue.password} onChange={handleInputOnChange}/>
+            <span>{inputMessage.password}</span>
+        </div>
+        <button disabled={signinButtonDisabled} onClick={handleSingninOnClick}>로그인</button>
+    </>
+}
+
+export default Auth02;
